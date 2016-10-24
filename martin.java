51218a3321f8aa3c2b827f3f5d6c8a1383a0d133
Martin Johansson <martin@backsgard.com>
2016-10-24 23:59:24 +0200

    Inital comit

---
+++ martin.java
@@ -0,0 +1,25 @@
+public class Switch 
+{
+    public static void main(String[] args) 
+    {
+       char penaltyKick = 'C';
+       switch (penaltyKick) 
+       {
+         case 'L': System.out.println("Martin shoots to the left and scores!");
+         break; 
+         case 'C': System.out.println("Martiartinn shoots down the center, but the keeper blocks it!");
+         break;
+	 case 'F': System.out.println('Henrik pa aventyr');
+ 	 break;
+	 case 'J': System.out.println('Henrik pa aventyr igen');
+ 	 break;
+         default:
+         System.out.println("MrT is not in position...");
+
+        }
+
+      }
+  }
+
+
+# test line
