6174f2163b374730aa24cd89b0520a7bda6a9634
Martin Johansson <martin@backsgard.com>
2016-10-25 00:15:14 +0200

    Changed the case C to Martin

--- martin.java
+++ martin.java
@@ -7,7 +7,7 @@ public class Switch
        {
          case 'L': System.out.println("Martin shoots to the left and scores!");
          break; 
-         case 'C': System.out.println("Martiartinn shoots down the center, but the keeper blocks it!");
+         case 'C': System.out.println("Martin shoots down the center, but the keeper blocks it!");
          break;
 	 case 'F': System.out.println('Henrik pa aventyr');
  	 break;
