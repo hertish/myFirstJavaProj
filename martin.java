dde55507451c11708ce2fb13719e6cf46a398068
Martin Johansson <martin@backsgard.com>
2016-10-25 16:49:37 +0200

    added new method minfunction to the branch testing

--- martin.java
+++ martin.java
@@ -21,6 +21,17 @@ public class Switch
         }
 
       }
+      /** the snippet returns the minimum between two numbers */
+
+      public static int minFunction(int n1, int n2) 
+      {
+      int min;
+      if (n1 > n2)
+         min = n2;
+      else
+        min = n1;
+      return min; 
+     }
   }
 
 
